578af348b094512fe01969db48c784acdccb6c41
Salome Ranson <salomeranson@gmail.com>
2021-07-16 19:31:08 -0400

    added state to search bar text input

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -7,7 +7,7 @@ const NavBar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Kindred</a>
+                    <Link className="navbar-brand" to="/">Kindred</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -2,29 +2,44 @@ import React from 'react';
 import './SearchBar.css';
 
 
-const SearchBar = () => {
-    return(
-        <div>
-            <div className="row" style={{ marginTop:"3%", marginLeft:"3%", marginRight:"3%" }}>
-                <div className="input-group mb-3">
-                    <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Music</a></li>
-                            <li><a className="dropdown-item" href="#">Movies</a></li>
-                            <li><a className="dropdown-item" href="#">TV Shows</a></li>
-                            <li><a className="dropdown-item" href="#">Podcasts</a></li>
-                            <li><a className="dropdown-item" href="#">Books</a></li>
-                            <li><a className="dropdown-item" href="#">Authors</a></li>
-                            <li><a className="dropdown-item" href="#">Games</a></li>
-                        </ul>
-                    <input type="text" className="form-control" aria-label="Text input with dropdown button" />
-                    <div style={{ marginLeft:"0.5%"}}>
-                        <button type="button" class="btn btn-dark">Search</button>
+class SearchBar extends React.Component {
+
+ state = { term: ''};
+
+    // onSearchBtnClick() {
+    //     console.log('CLICKED!');
+    // }
+
+    render() {
+        return(
+            <div>
+                <div className="row" style={{ marginTop:"3%", marginLeft:"3%", marginRight:"3%" }}>
+                    <div className="input-group mb-3">
+                        <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" value="music" >Music</a></li>
+                                <li><a className="dropdown-item" value="movies">Movies</a></li>
+                                <li><a className="dropdown-item" value="tvShows">TV Shows</a></li>
+                                <li><a className="dropdown-item" value="podcasts">Podcasts</a></li>
+                                <li><a className="dropdown-item" value="books">Books</a></li>
+                                <li><a className="dropdown-item" value="authors">Authors</a></li>
+                                <li><a className="dropdown-item" value="games">Games</a></li>
+                            </ul>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            aria-label="Text input with dropdown button" 
+                            value={this.state.term} 
+                            onChange={(e) => this.setState({ term: e.target.value })} 
+                        />
+                        <div style={{ marginLeft:"0.5%"}}>
+                            <button type="button" className="btn btn-dark" onClick={this.onSearchBtnClick}>Search</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
 
