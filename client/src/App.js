4de53e39761a20ebdeb77c83bb74ac2dd9bbdd8f
Salome Ranson <salomeranson@gmail.com>
2021-07-16 16:51:01 -0400

    added Bootstrap to public/index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NavBar from './components/NavBar';
 
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <NavBar />
     </div>
   );
 }
