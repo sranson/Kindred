679194876d264e889a9bd6522ce12188fef74282
Salome Ranson <salomeranson@gmail.com>
2021-07-17 13:02:49 -0400

    successfully retrieving data from api call and cleaned up search input filed behavior

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import axios from 'axios';
+// import axios from 'axios';
 import './SearchBar.css';
 
 
@@ -8,22 +8,28 @@ class SearchBar extends React.Component {
  state = { searchTerm: '', searchCategory: ''};
 
  onSearchBtnClick = () => {
-    // Make the call to tastedive api
     let term = this.state.searchTerm;
     let category = this.state.searchCategory;
-
-    axios.get('http://tastedive.com/api/similar?', {
-        params: {
-            q: term,
-            type: category,
-            info: 1,
-            limit: 5,
-            k: '419186-Similari-8DTISBRP'
-        },
-        headers: {
-            Authorization: ''
-        }
-    })
+    console.log(`Category: ${category}`);
+    console.log(`Search Term: ${term}`);
+    // axios.get('https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar', {
+    //     params: {
+    //         q: term,
+    //         type: category,
+    //         info: 1,
+    //         limit: 10,
+    //     },
+    //     headers: {
+    //         Authorization: ''
+    //     }
+    // }).then(results => { 
+    //     // console.log(results);
+    //     console.log(results.data.Similar.Info);
+    //     // console.log(results.similar.results); 
+    // }).catch((err) => {
+    //     console.log(err);
+    // })
+    this.setState({ searchTerm: '' });                                              // Clear out the input text after 'Search' button is clicked
  }
 
     render() {
@@ -31,15 +37,15 @@ class SearchBar extends React.Component {
             <div>
                 <div className="row" style={{ marginTop:"3%", marginLeft:"3%", marginRight:"3%" }}>
                         <div className="input-group mb-3">
-                            <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+                            <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{this.state.searchCategory}</button>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" value="music" onClick={(e) => this.setState({ searchCategory: e.target.outerText })}></a></li>
-                                    <li><a className="dropdown-item" value="movies" onClick={(e) => this.setState({ searchCategory: e.target.outerText })}>Movies</a></li>
-                                    <li><a className="dropdown-item" value="tvShows" onClick={(e) => this.setState({ searchCategory: e.target.outerText })}>Series</a></li>
-                                    <li><a className="dropdown-item" value="podcasts" onClick={(e) => this.setState({ searchCategory: e.target.outerText })}>Podcasts</a></li>
-                                    <li><a className="dropdown-item" value="books" onClick={(e) => this.setState({ searchCategory: e.target.outerText })}>Books</a></li>
-                                    <li><a className="dropdown-item" value="authors" onClick={(e) => this.setState({ searchCategory: e.target.outerText })}>Authors</a></li>
-                                    <li><a className="dropdown-item" value="games" onClick={(e) => this.setState({ searchCategory: e.target.outerText })}>Games</a></li>
+                                    <li><a className="dropdown-item" onClick={(e) => this.setState({ searchCategory: 'Music' })}>Music</a></li>
+                                    <li><a className="dropdown-item" onClick={(e) => this.setState({ searchCategory: 'Movies' })}>Movies</a></li>
+                                    <li><a className="dropdown-item" onClick={(e) => this.setState({ searchCategory: 'shows' })}>Shows</a></li>
+                                    <li><a className="dropdown-item" onClick={(e) => this.setState({ searchCategory: 'Podcasts' })}>Podcasts</a></li>
+                                    <li><a className="dropdown-item" onClick={(e) => this.setState({ searchCategory: 'Books' })}>Books</a></li>
+                                    <li><a className="dropdown-item" onClick={(e) => this.setState({ searchCategory: 'Authors' })}>Authors</a></li>
+                                    <li><a className="dropdown-item" onClick={(e) => this.setState({ searchCategory: 'Games' })}>Games</a></li>
                                 </ul>
                             <input 
                                 type="text" 
