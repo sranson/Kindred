ca28aed4bfab34eb8369f8646cdaddb0ff3c6b0f
Salome Ranson <salomeranson@gmail.com>
2021-07-17 16:46:14 -0400

    updated Card component from Link back to a href so that wikepedia link open correctly

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Card = (props) => {
   return (
@@ -9,7 +9,7 @@ const Card = (props) => {
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.description}...</p>
-                <Link to={props.moreInfo} className="btn btn-primary">Read More</Link>
+                <a href={props.moreInfo} target="_blank" className="btn btn-primary">Read More</a>
             </div>
           </div>
       </div>
