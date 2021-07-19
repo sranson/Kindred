bc6429b97960566ee7ef61479b7e00f9ee780730
Karen Roh <karenroh@hotmail.com>
2021-07-19 13:35:36 -0400

    successfully logging in user with token

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -22,6 +22,23 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No profile with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
   },
 };
 
