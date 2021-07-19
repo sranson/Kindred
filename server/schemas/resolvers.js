5fcaa7e2554bf57b735f32315144eea751201245
Karen Roh <karenroh@hotmail.com>
2021-07-19 13:27:31 -0400

    successfully adding user and returning a user token

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,9 @@
 //need to use the context middleware from the Apollo Server configured in the server file and pass it in as the third arguement for all requests where authorization is required. Example would be saving a category and deleting it
 //context is like a sessionId or cookie for a user
 
+const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -9,7 +11,18 @@ const resolvers = {
       return User.find();
     },
   },
-  Mutation: {},
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+
+      if (!user) {
+        throw new AuthenticationError("Something is wrong!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
