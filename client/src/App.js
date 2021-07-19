cb70d3998e08f27d5e4754e9ebb2d29180be4467
Karen Roh <karenroh@hotmail.com>
2021-07-19 11:29:08 -0400

    added ApolloProvider to App.js to attatch the JWT token to every request where user needs to be authorized

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,75 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
+import NavBar from "./components/NavBar";
+import Home from "./components/Home/index";
+import Profile from "./components/Profile";
+import SearchBar from "./components/SearchBar";
+import Matches from "./components/Matches";
+import Settings from "./components/Settings";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import NavBar from './components/NavBar';
-import Home from './components/Home/index';
-import Profile from './components/Profile';
-import SearchBar from './components/SearchBar';
-import Matches from './components/Matches';
-import Settings from './components/Settings'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
           <div>
-            {/* Define routes to render different page components at different paths */}
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <NavBar />
-            <Route exact path="/profile">
-              <Profile />
-            </Route>
-            <Route exact path="/search">
-              <SearchBar />
-            </Route>
-            <Route exact path="/matches">
-              <Matches />
-            </Route>
-            <Route exact path="/settings">
-              <Settings />
-            </Route>
+            <div>
+              {/* Define routes to render different page components at different paths */}
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <NavBar />
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+              <Route exact path="/search">
+                <SearchBar />
+              </Route>
+              <Route exact path="/matches">
+                <Matches />
+              </Route>
+              <Route exact path="/settings">
+                <Settings />
+              </Route>
+            </div>
           </div>
-        </div>
-      </Switch>
-    </Router>
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
