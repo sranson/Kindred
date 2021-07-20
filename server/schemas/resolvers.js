593d57d0bbcf85781754c34923ef9113e09acc92
Karen Roh <karenroh@hotmail.com>
2021-07-20 12:25:29 -0400

    added me query in resolvers to get my personal profile

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,6 +10,12 @@ const resolvers = {
     users: async () => {
       return User.find();
     },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
