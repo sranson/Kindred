8706122c38bd51390f8d4061dfe9d5ab84f9b58b
Karen Roh <karenroh@hotmail.com>
2021-07-20 22:30:58 -0400

    using useState to take care of onChange event for input fields

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,42 +1,81 @@
-import React from 'react';
-import './signup.css'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form'
+import React, { useState } from "react";
+import "./signup.css";
+import { Form, Button } from "react-bootstrap";
 
-export default function Login() {
-    return (
-        <main className='main'>
-            <h1 className='signUpTitle'>
-                <p>Kindred</p>
-            </h1>
-            <p className='signUpSubTitle'>
-                <p>Spark an Interest</p>
-            </p>
-            <div>
-                <Form>
-                    <Form.Group id='username' className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control id='inputUser' type="username" placeholder="Enter username" />
-                    </Form.Group>
-                    <Form.Group id='email' className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control id='inputEmail' type="email" placeholder="Enter email" />
-                        <Form.Text id='warning' className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+export default function Signup() {
+  // set initial form state
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-                    <Form.Group id='password' className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control id='inputPassword' type="password" placeholder="Password" />
-                    </Form.Group>
-                </Form>
-            </div>
-            <div id='button' className="d-grid gap-2">
-                <Button variant="secondary" size="lg">
-                    Sign Up
-                </Button>
-            </div>
-        </main>
-    );
+  const onChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <main className="main">
+      <h1 className="signUpTitle">
+        <p>Kindred</p>
+      </h1>
+      <p className="signUpSubTitle">
+        <p>Spark an Interest</p>
+      </p>
+      <div>
+        <Form onSubmit={onSubmit} noValidate>
+          <Form.Group id="username" className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              id="inputUser"
+              placeholder="Enter username"
+              type="text"
+              name="username"
+              value={values.username}
+              onChange={onChange}
+            />
+          </Form.Group>
+          <Form.Group id="email" className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              id="inputEmail"
+              type="email"
+              placeholder="Your email address"
+              name="email"
+              value={values.email}
+              onChange={onChange}
+            />
+            <Form.Text id="warning" className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group
+            id="password"
+            className="mb-3"
+            controlId="formBasicPassword"
+          >
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              id="inputPassword"
+              type="password"
+              placeholder="Your password"
+              name="password"
+              value={values.password}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Form>
+      </div>
+      <div id="button" className="d-grid gap-2">
+        <Button type="submit" variant="secondary" size="lg">
+          Sign Up
+        </Button>
+      </div>
+    </main>
+  );
 }
