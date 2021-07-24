a613cbd7e5bd32c062ac9ed3a35868ccaa22f54d
Karen Roh <karenroh@hotmail.com>
2021-07-23 21:19:10 -0400

    successfully rendering search result card and similar result card on profile page based on category type for music and movies

--- client/src/components/Card/SimilarResultCard.js
+++ client/src/components/Card/SimilarResultCard.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
-import { useMutation } from '@apollo/client';
-import { SAVE_CATEGORY } from '../../utils/mutations';
+import React, { useState, useEffect } from "react";
+import { useMutation } from "@apollo/client";
+import { SAVE_CATEGORY } from "../../utils/mutations";
 
 const SimilarResultCard = (props) => {
-
   const [saveCategory] = useMutation(SAVE_CATEGORY);
-  const [text, setText] = useState("Save")
+  const [text, setText] = useState();
 
-  const changeBtnText = () => {
-    setText("SAVED!")
-  }
+  useEffect(() => {
+    console.log("SIMILAR RESULT CARD HERE")
+    if (props.profile) {
+      setText("DELETE");
+    } else {
+      setText("SAVE");
+    }
+  }, []);
+
+  const changeBtnText = (text) => {
+    setText(text);
+  };
 
   const saveTheCategory = (e) => {
     e.preventDefault();
-    changeBtnText()
+    changeBtnText("SAVED!");
     try {
       saveCategory({
         variables: {
@@ -21,50 +29,105 @@ const SimilarResultCard = (props) => {
           type: props.type,
           description: props.description,
           wikiUrl: props.moreInfo,
-          youtubeUrl: props.video
-        }
-      })
-    } catch(err) {
-      console.error(err)
+          youtubeUrl: props.video,
+        },
+      });
+    } catch (err) {
+      console.error(err);
     }
-  }
+  };
+
+  const deleteTheCategory = (e) => {
+    e.preventDefault();
+    changeBtnText("DELETED!");
+    try {
+      //removeCategory()
+      console.log("deleted haha");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   if (props.video !== null) {
     return (
-        <div style={{ marginLeft: "4%" }}>
-            <div className="card" style={{width: "18rem"}}>
-              <iframe width="285" height="315"
-                  src={props.video}>
-              </iframe>
-              <div className="card-body">
-                  <h5 className="card-title">{props.title}</h5>
-                  <p className="card-text">{props.description}...</p>
-                  <div style={{ display: "flex", justifyContent: "space-between" }}>
-                        <a href={props.moreInfo} target="_blank" className="btn btn-primary">Read More</a>
-                        <a href="#" target="_blank" className="btn btn-primary" onClick={saveTheCategory}>{text}</a>
-                  </div>
-              </div>
+      <div style={{ marginLeft: "4%" }}>
+        <div className="card" style={{ width: "18rem" }}>
+          <iframe width="285" height="315" src={props.video}></iframe>
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.description}...</p>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <a
+                href={props.moreInfo}
+                target="_blank"
+                className="btn btn-primary"
+              >
+                Read More
+              </a>
+              {props.profile ? (
+                <a
+                  href="#"
+                  target="_blank"
+                  className="btn btn-primary"
+                  onClick={deleteTheCategory}
+                >
+                  {text}
+                </a>
+              ) : (
+                <a
+                  href="#"
+                  target="_blank"
+                  className="btn btn-primary"
+                  onClick={saveTheCategory}
+                >
+                  {text}
+                </a>
+              )}
             </div>
+          </div>
         </div>
-    )
+      </div>
+    );
   } else {
     return (
-        <div style={{ marginLeft: "4%" }}>
-            <div className="card" style={{width: "18rem"}}>
-              <div className="card-body">
-                  <h5 className="card-title">{props.title}</h5>
-                  <p className="card-text">{props.description}...</p>
-                  <div style={{ display: "flex", justifyContent: "space-between" }}>
-                        <a href={props.moreInfo} target="_blank" className="btn btn-primary">Read More</a>
-                        <a href="#" target="_blank" className="btn btn-primary" onClick={saveTheCategory}>{text}</a>
-                  </div>
-              </div>
+      <div style={{ marginLeft: "4%" }}>
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.description}...</p>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <a
+                href={props.moreInfo}
+                target="_blank"
+                className="btn btn-primary"
+              >
+                Read More
+              </a>
+              {props.profile ? (
+                <a
+                  href="#"
+                  target="_blank"
+                  className="btn btn-primary"
+                  onClick={deleteTheCategory}
+                >
+                  {text}
+                </a>
+              ) : (
+                <a
+                  href="#"
+                  target="_blank"
+                  className="btn btn-primary"
+                  onClick={saveTheCategory}
+                >
+                  {text}
+                </a>
+              )}
             </div>
+          </div>
         </div>
-    )
+      </div>
+    );
   }
-
-
 };
 
 export default SimilarResultCard;
--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./profile.css";
+import Card from "../Card/index";
+import SimilarResultCard from "../Card/SimilarResultCard";
 
 import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../../utils/queries";
@@ -33,10 +35,60 @@ class Collapsible extends React.Component {
 
 const Profile = () => {
   const { loading, data } = useQuery(QUERY_ME);
-
+  console.log(data);
   if (loading) {
     return <h2>Loading...</h2>;
   }
+  const filteredCategory = (chosenCategory) => {
+    const filter = data.me.savedCategories.filter((category) => {
+      return category.type === chosenCategory;
+    });
+
+    console.log(filter);
+
+    return filter;
+  };
+
+  const createCardGrid = (chosenCategory) => {
+    return (
+      <div>
+        {data.me.savedCategories.length > 0 ? (
+          <ul>
+            {filteredCategory(chosenCategory).map((category) => {
+              if (category.youtubeUrl === null) {
+                return (
+                  <Card
+                    type={category.type}
+                    image={category.image}
+                    title={category.title}
+                    description={category.description}
+                    moreInfo={category.wikiUrl}
+                    profile={true}
+                    key={category.title}
+                  ></Card>
+                );
+              } else {
+                return (
+                  <SimilarResultCard
+                    type={category.type}
+                    image={category.image}
+                    title={category.title}
+                    description={category.description}
+                    moreInfo={category.wikiUrl}
+                    video={category.youtubeUrl}
+                    profile={true}
+                    key={category.title}
+                  ></SimilarResultCard>
+                );
+              }
+            })}
+          </ul>
+        ) : (
+          <div className="ml-2">No interest saved for this category!</div>
+        )}
+      </div>
+    );
+  };
 
   return (
     <div className="profile">
@@ -52,18 +104,8 @@ const Profile = () => {
       </div>
       <div className="about">I just really love {data.me.username}</div>
       <div className="interests">
-        <Collapsible title="Music">
-          <ul>
-            <li>Butthole Surfers</li>
-            <li>Ween</li>
-          </ul>
-        </Collapsible>
-        <Collapsible title="Movies">
-          <ul>
-            <li>Airplane</li>
-            <li>Troll 2</li>
-          </ul>
-        </Collapsible>
+        <Collapsible title="Music">{createCardGrid("Music")}</Collapsible>
+        <Collapsible title="Movies">{createCardGrid("Movies")}</Collapsible>
         <Collapsible title="TV Shows">
           <ul>
             <li>I Think You Should Leave</li>
