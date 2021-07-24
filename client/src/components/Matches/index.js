ee35cf280da740dbc6232afb118df5df56bb7b7b
klucas72 <klucas72@hotmail.com>
2021-07-24 13:46:00 -0400

    made modification to css and styling

--- client/src/components/Matches/index.js
+++ client/src/components/Matches/index.js
@@ -1,37 +1,37 @@
-// import React from 'react';
-// import { useQuery, gql } from '@apollo/client';
-// import { QUERY_MATCHES } from '../../utils/queries';
-// import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { QUERY_MATCHES } from '../../utils/queries';
+import { useParams } from 'react-router-dom';
+
+const Matches = () => {
+    // const Matches = () => {
+    //     const { username } = useParams();
+    //     const { savedCategory } = useParams();
+
+    //     const { loading, data } = useQuery(QUERY_MATCHES, {
+    //         variables: { username: username, savedCategory: savedCategory },
+    //     },
+    //         console.log(data)
+    //     );
+    //     const matches = data.users.username || {};
+
+    //     if (loading) {
+    //         return <div>Loading...</div>
+    //     }
 
 
-// const Matches = () => {
-//     const { username } = useParams();
-//     const { savedCategory } = useParams();
-
-//     const { loading, data } = useQuery(QUERY_MATCHES, {
-//         variables: { username: username, savedCategory: savedCategory },
-//     },
-//         console.log(data)
-//     );
-//     const matches = data.users.username || {};
-
-//     if (loading) {
-//         return <div>Loading...</div>
-//     }
+    return (
+        <main>
+            <h1 className="card-header">
+                <Matches />
+            </h1>
+        </main>
+    )
 
 
-//     return (
-//         <main>
-//             <h1 className="card-header">
-//                 <Matches matches={matches.userName.savedCategory} />
-//             </h1>
-//         </main>
-//     )
+}
 
 
-// }
-
-
-// export default Matches;
+export default Matches;
 
 
