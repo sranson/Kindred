de6486b08fca19069ee405d45a8b3db93f4e7ffb
Salome Ranson <salomeranson@gmail.com>
2021-07-28 10:40:57 -0400

    search results are rendering horizontally

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -58,22 +58,44 @@ const SearchBar = () => {
     } else {
       return(
         <div>
+        <div className="row" style={{ marginTop: "3%", marginLeft: "3%", marginRight: "3%" }}>
+        <div className="input-group mb-3">
+          <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{searchCategory.toUpperCase()}</button>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" onClick={(e) => setSearchCategory("Music")}>Music</a></li>
+            <li><a className="dropdown-item" onClick={(e) => setSearchCategory("Movies")}>Movies</a></li>
+            <li><a className="dropdown-item" onClick={(e) => setSearchCategory("shows")}>Shows</a></li>
+            <li><a className="dropdown-item" onClick={(e) => setSearchCategory("books")}>Books</a></li>
+            <li><a className="dropdown-item" onClick={(e) => setSearchCategory("Authors")}>Authors</a></li>
+            <li><a className="dropdown-item" onClick={(e) => setSearchCategory("Games")}>Games</a></li>
+            <li><a className="dropdown-item" onClick={(e) => setSearchCategory("podcasts")}>Podcasts</a></li>
+          </ul>
+          <input type="text" className="form-control" aria-label="Text input with dropdown button" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+          <div style={{ marginLeft: "0.5%" }}>
+            <button type="button" className="btn btn-dark" onClick={onSearchBtnClick}>Search</button>
+          </div>
+        </div>
+      </div>
+        
+        <div className="row" style={{ marginLeft: "3%", marginRight: "3%" }}>
             {resultsArray.map((result) => {
               if (result.yUrl) {
                 return(
-                  <div >
+                  <div className="col-md-3" style={{ marginTop: "3%", marginBottom: "3%" }}>
                     <SimilarResultCard type={result.Type} video={result.yUrl} title={result.Name} description={result.wTeaser} moreInfo={result.wUrl} /> 
                   </div>
                 )
               } else if (!result.video) {
                 return(
-                  <div >
+                  <div className="col-md-3" style={{ marginTop: "3%", marginBottom: "3%" }}>
                     <Card type={result.Type} title={result.Name} description={result.wTeaser} moreInfo={result.wUrl} /> 
                   </div>
                 )
               }
             })}
         </div>
+
+      </div>
       )
     }
 }
--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://sranson:55Vtca2agHx5biEk@cluster0.kvcyt.mongodb.net/kindred?retryWrites=true&w=majority",
+    "mongodb://localhost/kindred",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
