632feae1b2db09f80fd6ef50788ab75aced39b71
Salome Ranson <salomeranson@gmail.com>
2021-09-03 10:27:02 -0400

    fixed deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 require(`dotenv`).config();
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://sranson:gf684xgoe7SJC0wQ@cluster0.kvcyt.mongodb.net/kindred?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/kindred", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
